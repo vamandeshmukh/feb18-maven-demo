f9c9d7f661b49bb8e4cf058e24462c58ef366fca
vamandeshmukh <vamandeshmukh@gmail.com>
2022-03-08 11:27:52 +0530

    Update AppTest.java

--- src/test/java/feb18/maven/demo/AppTest.java
+++ src/test/java/feb18/maven/demo/AppTest.java
@@ -2,7 +2,6 @@ package feb18.maven.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
