0bbb487c40b9bc0210e5ef790db1093611367a8b
vamandeshmukh <vamandeshmukh@gmail.com>
2022-03-10 12:50:53 +0530

    comparable

--- src/main/java/feb18/maven/demo/col/ComparableDemo.java
+++ src/main/java/feb18/maven/demo/col/ComparableDemo.java
@@ -8,18 +8,23 @@ public class ComparableDemo {
 	public static void main(String[] args) {
 
 		ArrayList<Employee> empList = new ArrayList<>();
-		empList.add(new Employee(103, "Sonu", 50000));
-		empList.add(new Employee(101, "Monu", 40000));
-		empList.add(new Employee(104, "Tonu", 60000));
-		empList.add(new Employee(102, "Ponu", 55000));
+		empList.add(new Employee(103, "Sonu", 50000.25));
+		empList.add(new Employee(101, "Monu", 40000.75));
+		empList.add(new Employee(104, "Tonu", 60000.50));
+		empList.add(new Employee(102, "Ponu", 55000.125));
 
 		System.out.println("Initial empList");
 		empList.forEach(e -> System.out.println(e.toString()));
 
-//		Collections.sort(empList);
+		Collections.sort(empList);
 
 		System.out.println("after sorting empList");
 		empList.forEach(e -> System.out.println(e.toString()));
+	}
+
+	public ArrayList<Employee> sort(ArrayList<Employee> obj) {
+		// code
+		return obj;
 	}
 
 }
