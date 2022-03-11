c476ec0138dd8c20859dee7d22234391486fa907
vamandeshmukh <vamandeshmukh@gmail.com>
2022-03-11 15:30:16 +0530

    comparator

--- src/main/java/feb18/maven/demo/strm/StreamsDemo.java
+++ src/main/java/feb18/maven/demo/strm/StreamsDemo.java
@@ -9,6 +9,8 @@ public class StreamsDemo {
 
 	public static void main(String[] args) {
 
+		Employee emp = new Employee();
+
 		List<Employee> empList = new ArrayList<>();
 		empList.add(new Employee(101, "Sonu", 50000));
 		empList.add(new Employee(102, "Monu", 60000));
@@ -19,14 +21,33 @@ public class StreamsDemo {
 		System.out.println("Iterate and print elements with forEach and lambda: ");
 		empList.forEach(e -> System.out.println(e.toString()));
 
-		System.out.println("Employees stream with higher salary:");
-		Stream<Employee> empStream = empList.stream();
-		empStream.filter(e -> e.getSalary() >= 60000).forEach(e -> System.out.println(e.toString()));
+		System.out.println("Employees list with higher salary:");
+		List<Employee> empWithHigherSal = empList.stream().filter(e -> e.getSalary() >= 60000)
+				.collect(Collectors.toList());
 
-//		empStream.forEach(e -> System.out.println(e.toString()));
+		empWithHigherSal.forEach(e -> System.out.println(e.toString()));
 
-//		List<Employee> empWithHigherSal = empStream.collect(Collectors.toList());
-//
+		System.out.println("Employees list with higher salary:");
+//		List<Employee> empWithHigherSal = empList.stream().filter(e -> e.getSalary() >= 60000)
+//				.collect(Collectors.toList());
 //		empWithHigherSal.forEach(e -> System.out.println(e.toString()));
+
+		empList.stream().filter(e -> e.getSalary() >= 60000).collect(Collectors.toList())
+				.forEach(e -> System.out.println(e.toString()));
+
+		System.out.println("Employees list of first 3 employees: ");
+		empList.stream().limit(3).collect(Collectors.toList()).forEach(e -> System.out.println(e.toString()));
+
+		System.out.println("Employees list of last 3 employees: ");
+		empList.stream().skip(2).collect(Collectors.toList()).forEach(e -> System.out.println(e.toString()));
+
+		System.out.println("Employees list of 2nd, 3 rd employees: ");
+		empList.stream().skip(1).limit(2).collect(Collectors.toList()).forEach(e -> System.out.println(e.toString()));
+//		List<String> empUpper = empList.stream().map(e -> e.getFirstName().toUpperCase()));
+		System.out.println("\nUsing map() to change case");
+		List<Employee> nameInUpperCase = empList.stream().map(emp2 -> emp2 = null).collect((Collectors.toList()));
+
+		System.out.println(nameInUpperCase);
+
 	}
 }
