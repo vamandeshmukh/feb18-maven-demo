c33e1e72872cc53774484163aba9bb49c761c746
vamandeshmukh <vamandeshmukh@gmail.com>
2022-03-16 10:08:13 +0530

    contraints

--- cg18feb.sql
+++ cg18feb.sql
@@ -44,12 +44,52 @@ insert into emp2 (eid, first_name, salary) values (102, 'Monu', 60000);
 insert into emp2 (eid, first_name, salary) values (103, 'Tonu', 55000);
 insert into emp2 (eid, first_name, salary) values (104, 'Ponu', 40000);
 insert into emp2 (eid, first_name, salary) values (105, 'Gonu', 65000);
+insert into emp2 (eid, first_name, salary) values (101, 'Ronu', 70000);
+insert into emp2 (eid, first_name, salary) values (null, 'Monu', 45000);
 
 commit; 
 
 SELECT * FROM emp2 WHERE salary > 50000;
 SELECT * FROM emp2 WHERE salary > 70000;
 
+-- Database 
+-- DBMS 
+-- RDBMS 
 
+-- OOP 
+-- Entity - class 
+-- features - fields 
+-- functions - methods 
 
+-- ER Model 
+-- Entity - table  
+-- features - columns 
+-- instance - row / record 
+
+-- Employee - works in - Department 
+
+-- SELECT ? FROM ? WHERE ? 
+
+-- RDBMS, PK, FK, Entity, Relationship
+
+DROP TABLE emp; 
+
+commit; 
+
+-- creating table without constraints
+-- CREATE TABLE dep(did INT, dname VARCHAR(10), city varchar(10));
+-- creating table WITH constraints
+CREATE TABLE dep(did INT CONSTRAINT dep_pk PRIMARY KEY, dname VARCHAR(10), city varchar(10));
+
+SELECT * FROM dep; 
+
+CREATE TABLE emp(
+	eid INT CONSTRAINT emp_pk PRIMARY KEY, 
+	ename VARCHAR(10), 
+	salary DECIMAL, 
+	did INT CONSTRAINT emp_dep_fk REFERENCES dep(did));
+
+SELECT * FROM emp; 
+
+INSERT INTO dep (did, dname, city) VALUES (10, 'HR', 'Hyderabad');
 
