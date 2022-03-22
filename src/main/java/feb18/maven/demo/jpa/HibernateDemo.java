3ee641cfdfa8b3c2140f20509988e04e69632c7d
vamandeshmukh <vamandeshmukh@gmail.com>
2022-03-22 09:43:20 +0530

    hibernate

--- src/main/java/feb18/maven/demo/jpa/HibernateDemo.java
+++ src/main/java/feb18/maven/demo/jpa/HibernateDemo.java
@@ -1,5 +1,31 @@
 package feb18.maven.demo.jpa;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import feb18.maven.demo.jpa.entity.Employee;
+
 public class HibernateDemo {
 
+	public static void main(String[] args) {
+
+		System.out.println("Start");
+
+		Configuration config = new Configuration();
+		SessionFactory sessionFactory = config.configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+
+		Employee emp = new Employee(106, "Tonu", 45000);
+		session.save(emp); // insert
+//		session.update(emp); // update 
+//		session.delete(emp); // delete 
+		transaction.commit();
+		Employee empData = session.get(Employee.class, 106); // select
+		System.out.println(empData.toString());
+		System.out.println("End");
+
+	}
 }
