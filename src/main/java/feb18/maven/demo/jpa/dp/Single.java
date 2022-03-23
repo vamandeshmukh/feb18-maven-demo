bc757fbe016153911ff92c9809be35cde1dde85b
vamandeshmukh <vamandeshmukh@gmail.com>
2022-03-23 10:04:19 +0530

    dp

--- src/main/java/feb18/maven/demo/jpa/dp/Single.java
+++ src/main/java/feb18/maven/demo/jpa/dp/Single.java
@@ -1,25 +1,38 @@
 package feb18.maven.demo.jpa.dp;
 
+//// lazy initialization - better memory management 
+//public class Single {
+//
+//	private static Single obj;
+//
+//	private Single() {
+//
+//	}
+//
+//	public static Single getInstance() {
+//		if (obj == null)
+//			obj = new Single();
+//		return obj;
+//	}
+//
+//}
+
+//early initialization - faster execution 
+
 public class Single {
 
-	private static Single obj;
+	private static Single obj = new Single();
 
 	private Single() {
 
 	}
 
-	public static Single getInstance() { // getInstance();
-		if (obj == null) {
-			obj = new Single();
-			return obj;
-
-		} else {
-			return obj;
-		}
+	public static Single getInstance() {
+		return obj;
 	}
 
-//	public static Single getSingle() { // getInstance();
-//	return new Single();
-//}
-
 }
+
+
+
+
